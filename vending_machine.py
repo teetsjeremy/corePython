efd5fc6b97ccef6f0925946da67f8856fbcb8e4c
Ubuntu <ubuntu@ip-172-31-21-222.us-west-2.compute.internal>
2019-11-12 22:00:42 +0000

    updated vending

--- vending_machine.py
+++ vending_machine.py
@@ -1,15 +1,22 @@
 from byotest import *
 
-usd_coins = [100, 50, 25, 10, 5, 1]
-eur_coins = [100, 50, 20, 10, 5, 2, 1]
+usd_dictionary = {1:20, 2:20, 5:20, 10:20, 20:20, 50:20, 100:20}
+eur_dictionary = {1:20, 5:20, 10:20, 25:20, 50:20, 100:20}
+
+usd_coins = {100:20, 50:20, 25:20, 10:20, 5:20, 1:20}
+eur_coins = {100:20, 50:20, 20:20, 10:20, 5:20, 2:20, 1:20}
 
 def get_change(amount, coins=eur_coins):
     
     change = []
-    for coin in coins:
-        while coin <= amount:
-            amount -= coin
-            change.append(coin)
+    for denomination in sorted(coins.keys(), reverse=True):
+        while denomination <= amount and coins[denomination] >0:
+            amount -= denomination
+            coins[denomination] -= 1
+            change.append(denomination)
+            
+    if amount != 0:
+        raise Exception("Insufficient coins to give change.")
     
     return change    
     
@@ -25,5 +32,7 @@ test_are_equal(get_change(3),[2,1])
 test_are_equal(get_change(7),[5,2])
 test_are_equal(get_change(9),[5,2,2])
 test_are_equal(get_change(35, usd_coins),[25,10])
+test_are_equal(get_change(5, {2: 1, 1: 4}), [2, 1, 1, 1])
+test_exception_was_raised(get_change, (5, {2: 1, 1: 2}), "Insufficient coins to give change.")
 
 print("All tests pass!")
